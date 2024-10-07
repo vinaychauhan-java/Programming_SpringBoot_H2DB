3f1436e9d7e2708f50f1920baee1b4a997836d52
Vinay Chauhan <vinaychauhan.java@gmail.com>
2024-10-07 16:12:15 -0300

    One-To-Many Artifacts for Spring Boot with H2DB

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,2 +1,6 @@
 insert into "User" ("id", "firstName", "lastName") values (101, 'Vipin', 'Chauhan');
 insert into "User" ("id", "firstName", "lastName") values (102, 'Vinay', 'Chauhan');
+
+insert into "Post" ("id", "comment", "user_id") values (1001, 'Vipin @ Comments - 1', '101');
+insert into "Post" ("id", "comment", "user_id") values (1002, 'Vinay @ Comments - 1', '102');
+insert into "Post" ("id", "comment", "user_id") values (1003, 'Vinay @ Comments - 2', '102');
